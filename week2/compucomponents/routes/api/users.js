d02552d65a14841c9ba7ef854c07bc6d98f98078
AnderBalles98 <ander.ballesteros98@gmail.com>
2020-11-16 13:44:09 -0500

    delete everything is not about week3

--- week2/compucomponents/routes/api/users.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var userControlerAPI = require("../../controllers/api/usersControlletAPI");
-
-router.get("/", userControlerAPI.user_list);
-router.post("/create", userControlerAPI.user_create);
-router.post("/booking/:id", userControlerAPI.user_booking);
-
-module.exports = router;
