9adc62e40517dea88f60d287a798897ca476780d
AnderBalles98 <ander.ballesteros98@gmail.com>
2020-11-16 13:16:08 -0500

    token create in mongoDb

--- week3/compucomponents/controllers/api/authControllerAPI.js
+++ week3/compucomponents/controllers/api/authControllerAPI.js
@@ -1,22 +1,27 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../../models/user");
+const Token = require("../../models/token");
 
 module.exports = {
         authenticate: function (req, res, next) {
-            console.log("hola mundo");
             const data = req.body;
-            console.log(data);
             User.findOne({email: data.email}, function(err, user) {
                 if (err) {
                    return next(err);
                 } else if (!user) {
                     return res.json({status: 404, message:"user not found"});
                 } else if(bcrypt.compareSync(data.password, user.password)) {
-                    const token = jwt.sign({id: user._id}, req.app.get('secretKey'), {expiresIn: '7d'});
-                    return res.json({status:200, data: {user, token}})
+                    let token = new Token({ userId: user._id, token: jwt.sign({id: user._id}, req.app.get('secretKey'), {expiresIn: '7d'})});
+                    token.save(function (err, token) {
+                        if (err) {
+                            return next(err);
+                        }
+                        return res.json({status:200, data: {user, token: token.token}})
+                    });
+                }else {
+                    return res.json({status:401, message:"invalid password"});
                 }
-                return res.json({status:401, message:"invalid password"});
 
             });
         }
